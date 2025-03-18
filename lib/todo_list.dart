4bc21453da507c98a5ed47aed217f70815ed18b5
Ahmed Mahmoud <ahmedmahmoudsaleh269@gmail.com>
2025-03-18 08:50:36 -0700

    added local storage feature using hive

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -1,57 +1,114 @@
+// lib/main.dart
 import 'package:flutter/material.dart';
-import 'package:todo_list_app/todo.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'todo.dart';
 
-import 'add_todo.dart';
+// ... existing code ...
 
 class TodoList extends StatefulWidget {
-  const TodoList({super.key});
-
   @override
-  State<TodoList> createState() => _TodoListState();
+  _TodoListState createState() => _TodoListState();
 }
 
 class _TodoListState extends State<TodoList> {
-  List<Todo> todos = [];
+  Box<Todo>? todoBox;
 
-  void addTodo(String title) {
-    setState(() {
-      todos.add(Todo(title: title));
-    });
+  @override
+  void initState() {
+    super.initState();
+    // Open the Hive box
+    todoBox = Hive.box<Todo>('todos');
+  }
+
+  void addTodo(String task) {
+    final newTodo = Todo(title: task);
+    todoBox?.add(newTodo); // Add new todo to the box
+    setState(() {}); // Refresh the UI
+  }
+
+  void editTodo(int index, String newTask) {
+    final todo = todoBox?.getAt(index);
+    if (todo != null) {
+      todo.title = newTask; // Update the task
+      todoBox?.putAt(index, todo); // Save changes to the box
+      setState(() {}); // Refresh the UI
+    }
   }
 
   void deleteTodo(int index) {
-    setState(() {
-      todos.removeAt(index);
-    });
+    todoBox?.deleteAt(index); // Delete the todo from the box
+    setState(() {}); // Refresh the UI
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('To_Do_List'),
+        title: Text('To-Do List'),
       ),
-      body: ListView.builder(
-          itemCount: todos.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(todos[index].title),
-              trailing: IconButton(
-                icon: Icon(Icons.delete),
-                onPressed: () => deleteTodo(index),
-              ),
-            );
-          }),
+      body: ValueListenableBuilder(
+        valueListenable: todoBox!.listenable(),
+        builder: (context, Box<Todo> box, _) {
+          return ListView.builder(
+            itemCount: box.length,
+            itemBuilder: (context, index) {
+              final todo = box.getAt(index);
+              return ListTile(
+                title: Text(todo!.title),
+                onTap: () {
+                  // Edit todo on tap
+                  _showEditDialog(index, todo.title);
+                },
+                trailing: IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () {
+                    deleteTodo(index); // Delete todo on button press
+                  },
+                ),
+              );
+            },
+          );
+        },
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final newTodo = await showDialog<String>(
-              context: context, builder: (context) => AddTodoDialog());
-          if (newTodo != null) {
-            addTodo(newTodo);
-          }
+        onPressed: () {
+          // Example of adding a todo
+          addTodo('New Task');
         },
         child: Icon(Icons.add),
       ),
+    );
+  }
+
+  void _showEditDialog(int index, String currentTask) {
+    final controller = TextEditingController(text: currentTask);
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Edit Todo'),
+          content: TextField(
+            controller: controller,
+            decoration: InputDecoration(hintText: 'Enter new task'),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                editTodo(index, controller.text); // Edit the todo
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text('Save'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text('Cancel'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
